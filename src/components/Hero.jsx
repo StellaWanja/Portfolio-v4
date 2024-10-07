3f5ba4d96a443e77138035d0d7b6224917726795
StellaWanja <stylades@gmail.com>
2024-10-07 11:19:15 +0300

    update: hero button positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "./Buttons/Button";
 
 const Hero = () => {
   return (
-    <section className="min-h-screen w-full flex flex-col sm:flex-row justify-center items-center sm:gap-5 -z-10 relative">
+    <section className="min-h-screen w-full flex flex-col sm:flex-row justify-center items-center gap-5 -z-10">
       <div className="w-full mx-auto flex flex-col items-center sm:items-start px-5  gap-3 ">
         <p className="text-2xl sm:text-3xl font-medium text-white font-generalsans">
           Hi, I&apos;m Stella | StylaDes <span className="waving-hand">👋</span>
@@ -16,7 +16,7 @@ const Hero = () => {
         <Spaceman />
       </div>
 
-      <div className="absolute bottom-[20%] left-0 right-0 w-full z-10 c-space">
+      <div className="absolute bottom-10 left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
           <Button isBeam containerClass="sm:w-fit w-full sm:min-w-96">
             Let&apos;s work together
